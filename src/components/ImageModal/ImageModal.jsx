7ec80445c34d6ae868fe1546b198b41059d450a8
Kateryna Kocheharova <kot41901@gmail.com>
2024-05-15 20:10:20 +0300

    sDAsf

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -13,7 +13,14 @@ const customStyles = {
     backgroundColor: "pink",
   },
   overlay: {
-    backgroundColor: "black",
+    overlay: {
+      position: "fixed",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: "rgba(255, 255, 255, 0.75)",
+    },
   },
 };
 
