e9d86e730358cf46e7b104d2430dbb5346a99c0d
Kateryna Kocheharova <kot41901@gmail.com>
2024-05-15 20:18:06 +0300

    bugfix - fix no overlay

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -13,14 +13,12 @@ const customStyles = {
     backgroundColor: "pink",
   },
   overlay: {
-    overlay: {
-      position: "fixed",
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      backgroundColor: "rgba(255, 255, 255, 0.75)",
-    },
+    position: "fixed",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "black",
   },
 };
 
