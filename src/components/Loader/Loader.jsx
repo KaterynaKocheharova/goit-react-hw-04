4952b9d346cc0b9854c8b8e8cbf5419e4f64b245
Kateryna Kocheharova <kot41901@gmail.com>
2024-05-22 23:19:30 +0300

    center loader wrapper

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,15 @@
 import { MagnifyingGlass } from "react-loader-spinner";
+import css from "./Loader.module.css";
 
 export default function Loader() {
   return (
-    <div>
+    <div className={css["loader-wrapper"]}>
       <p>Loading images. Please, wait</p>
       <MagnifyingGlass
         visible={true}
         height="80"
         width="80"
         ariaLabel="magnifying-glass-loading"
-        wrapperStyle={{}}
         wrapperClass="magnifying-glass-wrapper"
         glassColor="#c0efff"
         color="#e15b64"
